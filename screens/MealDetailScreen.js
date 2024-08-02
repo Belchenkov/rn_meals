a3553ee8f3ad916c384c0fa6a571e26c2464cb5b
Belchenkov <u608110@gmail.com>
2024-08-02 12:23:26 +0400

    Split mealdetail & styles

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,7 +1,9 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
 
 import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
+import SubTitle from "../components/MealDetail/SubTitle";
+import List from "../components/MealDetail/List";
 
 const MealDetailScreen = ({ route }) => {
 	const mealId = route.params.mealId;
@@ -9,26 +11,67 @@ const MealDetailScreen = ({ route }) => {
 	const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
 	return (
-		<View>
-			<Image source={{uri: selectedMeal.imageUrl}} />
-			<Text>{selectedMeal.title}</Text>
+		<ScrollView style={styles.rootContainer}>
+			<Image style={styles.image} source={{uri: selectedMeal.imageUrl}} />
+			<Text style={styles.title}>{selectedMeal.title}</Text>
 			<View>
 				<MealDetails
 					duration={selectedMeal.duration}
 					complexity={selectedMeal.complexity}
 					affordability={selectedMeal.affordability}
+					textStyle={styles.detailText}
 				/>
 			</View>
-			<Text>Ingredients</Text>
-			{selectedMeal.ingredients.map(ingredient => (
-				<Text key={ingredient}>{ingredient}</Text>
-			))}
-			<Text>Steps</Text>
-			{selectedMeal.steps.map(step => (
-				<Text key={step}>{step}</Text>
-			))}
-		</View>
+			<View style={styles.listOuterContainer}>
+				<View style={styles.listContainer}>
+					<SubTitle>Ingredients</SubTitle>
+					<List data={selectedMeal.ingredients} />
+
+					<SubTitle>Steps</SubTitle>
+					<List data={selectedMeal.steps} />
+				</View>
+			</View>
+		</ScrollView>
 	);
 };
 
 export default MealDetailScreen;
+
+const styles = StyleSheet.create({
+	rootContainer: {
+		marginBottom: 32,
+	},
+	image: {
+		width: '100%',
+		height: 350,
+	},
+	title: {
+		fontWeight: 'bold',
+		fontSize: 24,
+		margin: 8,
+		textAlign: 'center',
+		color: 'white'
+	},
+	detailText: {
+		color: 'white',
+	},
+	subTitle: {
+		color: '#e2b497',
+		fontSize: 18,
+		fontWeight: 'bold',
+		textAlign: 'center',
+	},
+	subTitleContainer: {
+		padding: 6,
+		marginHorizontal: 24,
+		marginVertical: 4,
+		borderBottomColor: '#e2b497',
+		borderBottomWidth: 2,
+	},
+	listOuterContainer: {
+		alignItems: 'center',
+	},
+	listContainer: {
+		width: '80%',
+	},
+});
